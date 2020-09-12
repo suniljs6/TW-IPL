e65cab252209418b979866d5eba0966516d68694
suniljs6 <suniljs2015@gmail.com>
2020-09-12 19:18:04 +0530

    fixed cors error and added styling to teams page as per design

--- main.js
+++ main.js
@@ -1,39 +1,31 @@
 var teamNames = {
     "chennai-super-kings": {
         id: "csk",
-        logo: "Chennai-Super-Kings-Logo-PNG.png"
     },
     "delhi-capitals": {
         id: "dc",
-        logo: "Delhi-Daredevils-Logo-PNG.png",
     },
     "kings-xi-punjab": {
         id: "punjab",
-        logo: "Kings-XI-Punjab-Logo-PNG.png",
     },
     "kolkata-knight-riders": {
         id: "kkr",
-        logo: "Kolkata-Knight-Riders-Logo-PNG.png",
     },
     "mumbai-indians": {
         id: "mi",
-        logo: "Mumbai-Indians-Logo-PNG.png",
     },
     "rajasthan-royals": {
         id: "rr",
-        logo: "Rajasthan-Royals-Logo-PNG.png",
     },
     "sunrisers-hyderabad": {
         id: "srh",
-        logo: "Sun-Risers-Logo-PNG.png"
     },
     "royal-challengers-bangalore": {
         id: "rcb",
-        logo: "Royal-Challengers-Bangalore-Logo-PNG.png"
     }
 }
 async function fetchTeamData(url){
-    let response = await fetch(url);
+    let response = await fetch("https://cors-anywhere.herokuapp.com/" +  url);
     if (response.ok) {
         let json = await response.json();
         return json;
@@ -49,22 +41,30 @@ function populateTeamData(){
             var name = document.createElement('div');
             name.id = team.id;
             name.innerHTML = team.teamName;
+            name.className += "team-name"
             toAdd.appendChild(name);
             var venue = document.createElement('div');
             venue.id = "venue";
             venue.innerHTML = team.venue;
+            venue.className += "team-venue"
             toAdd.appendChild(venue);
             if (team.winningYears.length) {
                 var cups = document.createElement('div');
                 cups.id = "cups";
-                cups.innerHTML =  team.winningYears.toString();
+                cups.className += "team-cups"
+                cups.innerHTML = "<i class='fa fa-trophy' style='color:yellow'></i> &nbsp"
+                cups.innerHTML +=  team.winningYears.join(", ");;
                 toAdd.appendChild(cups);
             }
             let teamData = document.getElementById(teamNames[team.id].id);
-            teamData.insertBefore(toAdd,teamData.childNodes[0]);
+            teamData.insertBefore(toAdd,teamData.childNodes[2]);
         });
         document.getElementById("loader-div").style.display = "none";
-        document.getElementById("teams").style.display = "flex";
+        document.getElementById("teams-div").style.display = "flex";
     })
+    
+}
+
+function viewTeam(team) {
     
 }
