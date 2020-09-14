8727fd840362011a27324324626dca97760a52b7
suniljs6 <suniljs2015@gmail.com>
2020-09-14 07:13:52 +0530

    checking mobile device

--- main.js
+++ main.js
@@ -110,30 +110,33 @@ function populateTeamData(){
     document.getElementById("loader-div").style.display = "flex";
     fetchTeamData("https://ipl-t20.herokuapp.com/teams").then(teams => {
         teams.forEach(team => {
-            console.log(setDimensions(),window.innerHeight);
-            var toAdd = document.createDocumentFragment();
-            var name = document.createElement('div');
-            name.id = team.id;
-            name.innerHTML = team.teamName;
-            name.className += "team-name"
-            toAdd.appendChild(name);
-            var venue = document.createElement('div');
-            venue.id = "venue";
-            venue.innerHTML = team.venue;
-            teamNames[team.id].venue = team.venue;
-            venue.className += "team-venue"
-            toAdd.appendChild(venue);
-            if (team.winningYears.length) {
-                var cups = document.createElement('div');
-                cups.id = "cups";
-                cups.className += "team-cups"
-                cups.innerHTML = "<i class='fa fa-trophy' style='color:yellow'></i> &nbsp"
-                cups.innerHTML +=  team.winningYears.join(", ");
-                teamNames[team.id].cups = team.winningYears.join(", ");
-                toAdd.appendChild(cups);
+            if (setDimensions()) {
+
+            } else {
+                var toAdd = document.createDocumentFragment();
+                var name = document.createElement('div');
+                name.id = team.id;
+                name.innerHTML = team.teamName;
+                name.className += "team-name"
+                toAdd.appendChild(name);
+                var venue = document.createElement('div');
+                venue.id = "venue";
+                venue.innerHTML = team.venue;
+                teamNames[team.id].venue = team.venue;
+                venue.className += "team-venue"
+                toAdd.appendChild(venue);
+                if (team.winningYears.length) {
+                    var cups = document.createElement('div');
+                    cups.id = "cups";
+                    cups.className += "team-cups"
+                    cups.innerHTML = "<i class='fa fa-trophy' style='color:yellow'></i> &nbsp"
+                    cups.innerHTML +=  team.winningYears.join(", ");
+                    teamNames[team.id].cups = team.winningYears.join(", ");
+                    toAdd.appendChild(cups);
+                }
+                let teamData = document.getElementById(teamNames[team.id].id);
+                teamData.insertBefore(toAdd,teamData.childNodes[2]);
             }
-            let teamData = document.getElementById(teamNames[team.id].id);
-            teamData.insertBefore(toAdd,teamData.childNodes[2]);
         });
         document.getElementById("loader-div").style.display = "none";
         document.getElementById("teams-div").style.display = "flex";
